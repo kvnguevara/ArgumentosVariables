a340e26717f7bc66292a9fa9c36024e86914c4ba
kevin <kevinguevararacarrasco120@gmail.com>
2021-04-16 23:16:46 +0200

    Proyecto final de curso medio de Java vesion 1.0

--- ProyectoPelicula/src/mx/com/gm/peliculas/excepciones/EscrituraDatosEx.java
+++ ProyectoPelicula/src/mx/com/gm/peliculas/excepciones/EscrituraDatosEx.java
@@ -7,20 +7,32 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class EscrituraDatosEx(){
+public class EscrituraDatosEx  extends AccesoDatosEx{
 
-    //MEtodos para que escriba en el archivo
+    //constructor sobrecargado
+    public EscrituraDatosEx(String mensaje){
+        super(mensaje);
+
+    }
+
+    public void crearArchivo(){
+
+    }
+
+    //MEtodos para que escriba la pelicula en el Txt
     public void escribirArchivo(String nombreArchivo, Pelicula pelicula)  {
         File file = new File(nombreArchivo);
         if (file.exists()){
             try {
-                PrintWriter pw = new PrintWriter(new FileWriter(file));
+                PrintWriter pw = new PrintWriter(new FileWriter(file),true);
+                pw.println(nombreArchivo);
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
         }else{
-
+            crearArchivo();
+            escribirArchivo(nombreArchivo,pelicula);
         }
     }
 
